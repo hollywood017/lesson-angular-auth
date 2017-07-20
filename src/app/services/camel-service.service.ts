d1fd886f5fa5441b35cae25a1f5739c1c18898bf
Nick Hoats <nhoatz@gmail.com>
2017-07-20 17:51:28 -0400

    finished camels service

--- src/app/services/camel-service.service.ts
+++ src/app/services/camel-service.service.ts
@@ -1,8 +1,50 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class CamelServiceService {
 
-  constructor() { }
+  constructor(
+    private httpThang: Http
+  ) { }
+
+
+  //POST camels
+  newCamels(theName, theColor, theHumps){
+    return this.httpThang
+    .post(
+      'http://localhost:3000/api/camels',
+
+      //Form body information to send to the back end (req.body)
+      {
+        camelName: theName,
+        camelColor: theColor,
+        camelHumps: theHumps
+      },
+
+      //Send the cookies across domains
+      { withCredentials: true }
+    )
+    //parse the JSON
+    .map(res => res.json());
+  }//close newCamels
+
+
+
+  //GET camels
+  allCamels() {
+    return this.httpThang
+    .get(
+      'http://localhost:3000/api/camels',
+
+      //Send the cookies across domains
+      { withCredentials: true }
+    )
+    //parse the JSON
+    .map(res => res.json());
+  }//close allCamels
+
 
 }
