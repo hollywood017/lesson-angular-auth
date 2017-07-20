614cf2da576922e89b95bcc5540b3a53e4c02cb4
Nick Hoats <nhoatz@gmail.com>
2017-07-20 17:36:26 -0400

    auth service done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
+import { AuthServiceService } from './services/auth-service.service';
+import { CamelServiceService } from './services/camel-service.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CamelListComponent } from './camel-list/camel-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CamelListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    AuthServiceService,
+    CamelServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
